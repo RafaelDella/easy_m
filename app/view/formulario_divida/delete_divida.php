24e23dc55697375972f78ce266e2a243b52ac95f
bruno sakaguchi <brunokioshi01@gmail.com>
2025-05-28 21:02:41 -0300

    Delete: pastas antigas

--- app/view/formulario_divida/delete_divida.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../../db.php';
-$bd = new DB();
-$conn = $bd->connect();
-
-$stmt = $conn->prepare("DELETE FROM Divida WHERE id_divida = ?");
-$stmt->execute([$_GET['id']]);
-
-header("Location: index.php?status=" . urlencode("Dívida excluída."));
